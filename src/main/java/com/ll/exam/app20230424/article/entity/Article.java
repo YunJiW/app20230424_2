cc8d9e90350f9b55ceff36e8a79f38e7bd9502bc
YunJiW <dtroy371@gmail.com>
2023-04-24 15:35:36 +0900

    도커라이즈 2단계

---
+++ src/main/java/com/ll/exam/app20230424/article/entity/Article.java
@@ -0,0 +1,20 @@
+package com.ll.exam.app20230424.article.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String subject;
+}
