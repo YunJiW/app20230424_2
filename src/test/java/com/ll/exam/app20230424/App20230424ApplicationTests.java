e7b8f384713fcee80d1ff0f1b190f8b526905494
YunJiW <dtroy371@gmail.com>
2023-04-24 20:16:56 +0900

    test profile 추가

--- src/test/java/com/ll/exam/app20230424/App20230424ApplicationTests.java
+++ src/test/java/com/ll/exam/app20230424/App20230424ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.exam.app20230424;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class App20230424ApplicationTests {
 
 	@Test
